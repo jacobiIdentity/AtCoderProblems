0d92bf65337c2c9a58386e7263a39a775dd6c9c5
jacobiIdentity <gurutan_kaiba_4334@yahoo.co.jp>
2025-09-07 01:11:15 +0900

    dp_n solved

--- dp/n/main.py
+++ dp/n/main.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-n,w = map(int,input().split())
-w_v = [list(map(int,input().split())) for _ in range(n)]
-dp =  [[0]*(w+1) for _ in range(n+1)]
-# print(w_v)
-for i in range(1,n+1) :
-    w_i,v_i = w_v[i-1][0],w_v[i-1][1]
-    if i == 1 :
-        dp[i][w_i] = v_i
-        continue
-    for j in range(w+1) :        
-        dp[i][j] = max(dp[i][j],dp[i-1][j])
-        if j+w_i<= w :
-            dp[i][j+w_i] = max(dp[i-1][j]+v_i,dp[i-1][j+w_i])
-print(max(dp[-1]))
+import itertools
+n = int(input())
+a_n = list(map(int,input().split()))
+acs =  list(itertools.accumulate([0]+a_n))
+dp = [[float('inf')]*(n+1) for _ in range(n)]
+for k in range(1,n+1) :
+    for i in range(n) :
+        if i+k > n :
+            break
+        if k== 1 :
+            dp[i][i+k] = 0
+            continue
+        for l in range(1,k) :
+            dp[i][i+k] = min(dp[i][i+k],dp[i][i+l]+dp[i+l][i+k]+acs[i+k]-acs[i])
+print(dp[0][-1])
 # print(dp)
+
